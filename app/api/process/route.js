3d1339cf77fbbb3aaef60515356fef2ef27033e3
Mubeen014 <mubeenshafqat14@gmail.com>
2024-08-27 03:21:19 +0500

    ready for deployment

--- app/api/process/route.js
+++ app/api/process/route.js
@@ -3,7 +3,8 @@ import { NextResponse } from 'next/server';
 export async function POST(request) {
   const { text } = await request.json();
   
-  const response = await fetch('http://127.0.0.1:5000/api/process', {
+  // const response = await fetch('http://127.0.0.1:5000/api/process', {
+    const response = await fetch('/api/process', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
